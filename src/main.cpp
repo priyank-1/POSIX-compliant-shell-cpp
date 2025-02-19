cefc812b23c145e571a6142c1b6a67080a2f644b
PRIYANK 1 <singhumang444@gmail.com>
2025-02-19 21:15:01 +0530

    Fifth Commit

--- src/main.cpp
+++ src/main.cpp
@@ -18,14 +18,9 @@ int main() {
     std::cout << "$ ";
     std::getline(std::cin,input);
     if(input == "exit 0") exit(0);
-    if(input == "echo"){
-      std::stringstream ss(input);
-      std::string cmd;
-      int cnt = 0;
-      while(ss >> cmd){
-        if(cnt >0) std::cout << cmd << " ";
-        cnt++;
-      }
+    if(input.find("echo ") == 0){
+      std::cout << input.substr(5) << std::endl;
+      continue;
     }
     std::cout << input << ": command not found" << std::endl;
   }
